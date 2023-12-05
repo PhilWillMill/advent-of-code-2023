ef64ca89fa121faeecbaa2d56c0ebb30da7ecec0
Phil Milligan <phil.milligan@safetyculture.io>
2023-12-05 11:39:00 +1100

    Solution for day 1 part 2

--- day1/day1.go
+++ day1/day1.go
@@ -9,17 +9,28 @@ import (
 	"strconv"
 )
 
-func reverseString(str string) (reversed string) {
-
-	for _, v := range str {
-		reversed = string(v) + reversed
+func stringToNumber(str string) (value string) {
+	numbers := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
 	}
-	return
+	if len(str) > 1 {
+		value = numbers[str]
+		return
+	}
+	return str
 }
 
 func main() {
 	result := 0
-	r, err := regexp.Compile("[1-9]")
+	r, err := regexp.Compile("one|two|three|four|five|six|seven|eight|nine|[1-9]")
 
 	file, err := os.Open("./input.txt")
 	if err != nil {
@@ -29,8 +40,17 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
+		line := scanner.Text()
 		first := r.FindString(scanner.Text())
-		last := r.FindString(reverseString(scanner.Text()))
+
+		var i int = len(line) - 1
+		last := ""
+		for last == "" {
+			last = r.FindString(line[i:])
+			i--
+		}
+		first = stringToNumber(first)
+		last = stringToNumber(last)
 		combined, _ := strconv.Atoi(first + last)
 		result = result + combined
 	}
